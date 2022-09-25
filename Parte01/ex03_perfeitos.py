4aa74c670c432cde5437aa4c28b3f27149b518ad
lclem0 <joaolclemente1@gmail.com>
2022-09-25 15:29:44 +0100

    commit

---
+++ Parte01/ex03_perfeitos.py
@@ -0,0 +1,16 @@
+maximum_number = 100
+
+def isPerfect(value):
+    # <Fill the blanks>
+    return False
+
+def main():
+    print("Starting to compute perfect numbers up to " + str(maximum_number))
+
+    for i in range(0, maximum_number):
+        if isPerfect(i):
+            print('Number ' + str(i) + ' is perfect.')
+
+
+if __name__ == "__main__":
+    main()
